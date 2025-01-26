ace78b33c8239fd681c145275845ebfeb634386c
dljardim <dljardim@outlook.com>
2025-01-26 11:47:12 -0600

    initial commit

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, eeee!")
         }
         .padding()
     }
@@ -22,3 +22,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
