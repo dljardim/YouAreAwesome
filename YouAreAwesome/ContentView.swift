43a2c9c9ca7ad24f89470793a0856665b2b8ae96
dljardim <dljardim@outlook.com>
2025-01-28 18:38:37 -0600

    testing github integration

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,7 +15,8 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, eeee!")
                 .fontWeight(Font.Weight.heavy)
-                .padding(61)  
+                .padding(61)
+                .font(Font.title)
         }
         .padding()
     }
