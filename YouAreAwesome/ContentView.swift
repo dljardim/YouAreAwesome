dcb430d6cd1d2a22501ff06c5583dc9852ec295f
dljardim <dljardim@outlook.com>
2025-01-31 12:32:05 -0600

    completed lesson - 1.2 -  struct variables cannot be change - view body variables can be changed - @State private var can changed - closure functions

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,15 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // struct var cannot be changed unless we add it to state
+    // private access modifier -
+    @State private var messageContentView = "ContentView child"
+    
+    
     var body: some View {
-        HStack {
+
+        // can be changed
+        var messageBody = "Body child"
+        
+        VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, eeee!")
+            Text("This changes")
                 .fontWeight(Font.Weight.heavy)
-                
                 .font(Font.title)
+            
+            Text(messageBody)
+
+            // trailing closure
+            Button("Click Me!"){
+                // direct children of the ContentView:View struct cannot be changed by the struct
+                messageContentView = "Awesome View"
+                
+                // the messageBody is not a direct child of the ContentView:View so it can be changed.
+                messageBody = "Awesome Body"
+                
+            }
         }
         .padding()
     }
