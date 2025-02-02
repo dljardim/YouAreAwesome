10c1fac49ea602eb92dd94905f0706c921e047cd
dljardim <dljardim@outlook.com>
2025-02-02 10:11:47 -0600

    challenge 1.12 - - while loop - repeat loop

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 
+
 import SwiftUI
 
 struct ContentView: View {
@@ -13,12 +14,7 @@ struct ContentView: View {
     // struct var cannot be changed unless we add it to state
     // private access modifier -
     @State private var messageNumber:Int = 0
-    let messages: [String] = ["You are Awesome!",
-                                             "You are amazing!",
-                                             "You are incredible!",
-                                             "Good on you",
-                                             "Way to Go",
-                                             "Incredible"]
+ 
     @State private var message = "";
     @State private var imageNumber = 0
     @State private var imageString = "image"
@@ -26,37 +22,68 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value:message)
             
             Image(imageString)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 .shadow(radius: 30)
-                            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .animation(.default, value:imageString)
             
             Spacer()
-
+            
             Button("Show Message"){
                 
-                message = messages[messageNumber]
-                messageNumber += 1
+                let messages: [String] = ["You are Awesome!",
+                                          "You are amazing!",
+                                          "If I were judging you from 1 to 10. You are most definately an Eleven",
+                                          "You are incredible!",
+                                          "Good on you",
+                                          "Way to Go",
+                                          "Incredibly Incredible"]
                 
-                if(messageNumber > messages.count - 1){
-                    messageNumber = 0
+
+                // while loop - inside loop only runs on conditional
+                var nextImageWhile = "image\(String(Int.random(in:0...9)))"
+                while(imageString == nextImageWhile){
+                    nextImageWhile = "image\(String(Int.random(in:0...9)))"
                 }
-             
-                if(imageNumber > 9){
-                    imageNumber = 0
-                }
+                imageString = nextImageWhile
                 
-                imageString = "image\(imageNumber)"
-                imageNumber += 1
-              
+                
+                // repeat - inside of repeat is executed at least ONCE
+                var nextImageRepeat = "image\(String(Int.random(in:0...9)))"
+                repeat{
+                    nextImageRepeat = "image\(String(Int.random(in:0...9)))"
+                } while(nextImageRepeat == imageString)
+                imageString = nextImageRepeat
+                          
+                print("imageString:",nextImageRepeat)
+                
+                
+                // while loop - messages
+                var nextMessageWhile = messages[(Int.random(in:0...(messages.count-1)))]
+                while(message == nextMessageWhile){
+                    nextMessageWhile = messages[(Int.random(in:0...(messages.count-1)))]
+                }
+                message = nextMessageWhile
+                
+                // repeat loop - messages
+                var nextMessageRepeat = messages[(Int.random(in:0...(messages.count-1)))]
+                repeat{
+                    nextMessageRepeat = messages[(Int.random(in:0...(messages.count-1)))]
+                } while(nextMessageRepeat == message)
+                message = nextMessageRepeat
+         
             }
             .buttonStyle(.borderedProminent)
             .tint(.orange)
